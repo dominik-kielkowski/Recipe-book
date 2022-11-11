77e245cb416e183facba8806af66635a69caa075
Dominik-Kielkowski <dominik0kielkowski@gmail.com>
2022-11-11 13:53:42 +0100

    new file:   .gitignore

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Recipe } from "./recipe.model";
+
+@Injectable()
+export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
+
+    private recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'This is a test description', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/800px-Eq_it-na_pizza-margherita_sep2005_sml.jpg',[new Ingredient('egg', 12),
+        new Ingredient('Meat', 24)]),
+        new Recipe('Another Test Recipe', 'This is a test description', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/800px-Eq_it-na_pizza-margherita_sep2005_sml.jpg',[new Ingredient('Milk', 432),
+        new Ingredient('Cheese', 64)])
+      ];
+
+      constructor(private slService: ShoppingListService) {
+
+      }
+
+      getRecipes(): Recipe[] {
+        return this.recipes.slice();
+      }
+
+      getRecipe(index: number){
+        return this.recipes[index];
+      }
+
+      addIngredientsToShoppingList(ingredients: Ingredient[]) {
+          this.slService.addIngredients(ingredients);
+      }
+
+      addRecipe(recipe: Recipe) {
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice());
+      }
+
+      updateRecipe(index: number, newRecipe: Recipe) {
+        this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.recipes.slice());
+      }
+
+      deleteRecipe(index: number){
+        this.recipes.splice(index, 1);
+        this.recipesChanged.next(this.recipes.slice());
+      }
+}
